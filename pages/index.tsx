8afba486acee805e46070fb0c1caaa652f952d19
searchme86 <searchme86@gmail.com>
2022-03-27 16:59:18 +0900

    ⚰️ 🗑️   removed  : console.log

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,7 @@ interface ProductsResponse {
 const Home: NextPage = () => {
   const { user, isLoading } = useUser();
   const { data } = useSWR<ProductsResponse>('/api/products');
+
   return (
     <Layout title="홈" hasTabBar>
       <Head>
